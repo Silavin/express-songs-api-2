582b860f0495b1bcc60d8f88ce80cc9fd71ec224
Sebastian Sim <s.sim97@hotmail.com>
2018-10-17 15:11:23 +0800

    completed

--- routes/songs.js
+++ routes/songs.js
@@ -1,63 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
 
 let songs = [];
 
 //Song API
 //return list of all songs
-router.get('/', (req, res, next) => {
-    res.status(200).json(songs);
+router.get("/", (req, res, next) => {
+  res.status(200).json(songs);
 });
 
 //create a new song, and return new song
-router.post('/', (req, res) => {
-    if (req.body){
-        let newSong = {
-            id: songs.length + 1,
-            name: req.body.name,
-            artist: req.body.artist 
-        }
-        songs.push(newSong);
-        res.status(201).json(newSong);
+router.post("/", (req, res) => {
+  const schema = Joi.object().keys({
+    name: Joi.string().required(),
+    artist: Joi.string().required()
+  });
+  Joi.validate(req.body, schema, (err, data) => {
+    if (err) {
+      next(new Error("Unable to create song"));
     }
-    next(new Error("Unable to create song"))
+    const newSong = { ...data, id: songs.length + 1 };
+    songs.push(newSong);
+    res.status(201).json(newSong);
+  });
 });
 
-//return a song with id 
-router.get('/:id', (req, res, next) => {
-
-    let song = songs.find(song => song.id == parseInt(req.params.id));
-    if(song) {
-        res.status(200).json(song);
-    }
-    next(new Error(`Unable to find song with id: ${req.params.id}`))
+//return a song with id
+router.get("/:id", (req, res, next) => {
+  let song = songs.find(song => song.id == parseInt(req.params.id));
+  if (song) {
+    res.status(200).json(song);
+  }
+  next(new Error(`Unable to find song with id: ${req.params.id}`));
 });
 
 //update a song with id, and return edited song
-router.put('/:id', (req, res, next) => {
-    let song = songs.find(song => song.id === parseInt(req.params.id));
-    if (song) {
-        song.name = req.body.name;
-        song.artist = req.body.artist;
-        res.status(200).json(song);
-    }
-    next(new Error(`Unable to update song with id: ${req.params.id}`))
+router.put("/:id", (req, res, next) => {
+  const song = songs.find(song => song.id === parseInt(req.params.id));
+  const schema = Joi.object().keys({
+    name: Joi.string().required(),
+    artist: Joi.string().required()
+  });
+
+  if (song) {
+    Joi.validate(req.body, schema, (err, data) => {
+      if (err) {
+        next(new Error(`Unable to update song with id: ${req.params.id}`));
+      }
+      song.name = data.name;
+      song.artist = data.artist;
+      res.status(200).json(song);
+    });
+  }
+
+  next(new Error(`Unable to update song with id: ${req.params.id}`));
 });
 
 //delete a song with id, and return deleted song
 router.delete("/:id", (req, res, next) => {
-    let songToDelete = songs.find(song => song.id === parseInt(req.params.id));
-    if(songToDelete){
-        let index = songs.indexOf(songToDelete);
-        songs.splice(index, 1);
-        res.status(200).json(songToDelete);
-    }
-    next(new Error(`Unable to delete song with id: ${req.params.id}`))
+  let songToDelete = songs.find(song => song.id === parseInt(req.params.id));
+  if (songToDelete) {
+    let index = songs.indexOf(songToDelete);
+    songs.splice(index, 1);
+    res.status(200).json(songToDelete);
+  }
+  next(new Error(`Unable to delete song with id: ${req.params.id}`));
 });
 
 //Add error handler for songs router to return 404 on failure at any route
 router.use((err, req, res, next) => {
-    res.status(404).json({ message: err.message });
+  res.status(404).json({ message: err.message });
 });
 
 module.exports = router;
--- tests/songs.test.js
+++ tests/songs.test.js
@@ -2,70 +2,70 @@ const app = require("../app");
 const request = require("supertest");
 
 describe("routes/songs", () => {
-
   it("POST /songs should return a new song object", () => {
-    requestBody = { name: "test song", artist: "rhianna"};
-    
+    requestBody = { name: "test song", artist: "rhianna" };
+
     return request(app)
-    .post("/songs")
-    .send(requestBody)
-    
-    .then(response => {
-      expect(response.status).toEqual(201);
-      expect(response.body).toMatchObject(requestBody);
-    });
+      .post("/songs")
+      .send(requestBody)
+
+      .then(response => {
+        expect(response.status).toEqual(201);
+        expect(response.body).toMatchObject(requestBody);
+      });
   });
-  
+
   it("GET /songs should return a non empty array", () => {
     return request(app)
-    .get("/songs")
-    
-    .then(response => {
-      expect(response.status).toEqual(200);
-      expect(Array.isArray(response.body)).toEqual(true);
-      expect(response.body.length).toBeGreaterThan(0);
-    });
+      .get("/songs")
+
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(Array.isArray(response.body)).toEqual(true);
+        expect(response.body.length).toBeGreaterThan(0);
+      });
   });
-  
+
   it("GET /songs/:id should return the song with id", () => {
-    expected = {id: 1, name: "test song", artist: "rhianna"};
-    
+    expected = { id: 1, name: "test song", artist: "rhianna" };
+
     return request(app)
-    .get("/songs/1")
-    .send(requestBody)
-    
-    .then(response => {
-      expect(response.status).toEqual(200);
-      expect(response.body).toMatchObject(expected);
-    });
+      .get("/songs/1")
+      .send(requestBody)
+
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body).toMatchObject(expected);
+      });
   });
 
   it("GET /songs/:id should return 404 if song id does not exist", () => {
     return request(app)
-    .get("/songs/10")
-    .send(requestBody)
-    
-    .then(response => {
-      expect(response.status).toEqual(404);
-      expect(response.body).toMatchObject({message: 'Unable to find song with id: 10' });
-    });
+      .get("/songs/10")
+      .send(requestBody)
+
+      .then(response => {
+        expect(response.status).toEqual(404);
+        expect(response.body).toMatchObject({
+          message: "Unable to find song with id: 10"
+        });
+      });
   });
 
   it("PUT /songs/id should return the updated song", () => {
     requestBody = {
-      id: 1,
       name: "updated song",
       artist: "rhianna"
     };
-    
+
     return request(app)
-    .put("/songs/1")
-    .send(requestBody)
-    
-    .then(response => {
-      expect(response.status).toEqual(200);
-      expect(response.body).toMatchObject(requestBody);
-    });
+      .put("/songs/1")
+      .send(requestBody)
+
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body).toEqual({ ...requestBody, id: 1 });
+      });
   });
 
   it("PUT /songs/:id should return 404 if song id does not exist", () => {
@@ -76,13 +76,15 @@ describe("routes/songs", () => {
     };
 
     return request(app)
-    .put("/songs/10")
-    .send(requestBody)
-    
-    .then(response => {
-      expect(response.status).toEqual(404);
-      expect(response.body).toMatchObject({message: 'Unable to update song with id: 10' });
-    });
+      .put("/songs/10")
+      .send(requestBody)
+
+      .then(response => {
+        expect(response.status).toEqual(404);
+        expect(response.body).toMatchObject({
+          message: "Unable to update song with id: 10"
+        });
+      });
   });
 
   it("DELETE /songs/:id should return the deleted song", () => {
@@ -93,33 +95,34 @@ describe("routes/songs", () => {
     };
 
     return request(app)
-    .delete("/songs/1")
+      .delete("/songs/1")
 
-    .then(response => {
-      expect(response.status).toEqual(200);
-      expect(response.body).toMatchObject(expected);
-    })
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body).toMatchObject(expected);
+      });
   });
-  
+
   it("DELETE /songs/:id should return 404 if song id does not exist", () => {
     return request(app)
-    .delete("/songs/10")
-    
-    .then(response => {
-      expect(response.status).toEqual(404);
-      expect(response.body).toMatchObject({message: 'Unable to delete song with id: 10' });
-    });
+      .delete("/songs/10")
+
+      .then(response => {
+        expect(response.status).toEqual(404);
+        expect(response.body).toMatchObject({
+          message: "Unable to delete song with id: 10"
+        });
+      });
   });
 
   it("GET /songs should return an empty array", () => {
     return request(app)
-    .get("/songs")
-    
-    .then(response => {
-      expect(response.status).toEqual(200);
-      expect(Array.isArray(response.body)).toBe(true);
-      expect(response.body.length).toEqual(0);
-    });
-  });
+      .get("/songs")
 
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toEqual(0);
+      });
+  });
 });
